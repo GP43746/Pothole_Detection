a89c187c3e891f8cf4531096bb7b17605c106627
GP43746 <gauranshpundir@gmail.com>
2026-01-02 22:28:01 +0530

    Update train_yolov8_baseline.py

--- train_yolov8_baseline.py
+++ train_yolov8_baseline.py
@@ -5,37 +5,29 @@ from multiprocessing import freeze_support
 
 
 def main():
-    # ----------------------------
-    # Safety checks
-    # ----------------------------
+   
     assert torch.cuda.is_available(), "CUDA not available. Check GPU setup."
     print("Using GPU:", torch.cuda.get_device_name(0))
 
-    # ----------------------------
-    # Paths
-    # ----------------------------
+   
     DATA_YAML = "dataset/yolo_v8/pothole.yaml"
     MODEL_WEIGHTS = "yolov8n.pt"
 
-    # ----------------------------
-    # Load model
-    # ----------------------------
+    
     model = YOLO(MODEL_WEIGHTS)
 
-    # ----------------------------
-    # Train
-    # ----------------------------
+   
     results = model.train(
     data=DATA_YAML,
     epochs=50,
     imgsz=640,
-    batch=8,              # safe for RTX 2050
+    batch=8,              
     optimizer="AdamW",
     lr0=0.001,
     weight_decay=0.0005,
     patience=15,
-    device=0,             # GPU
-    workers=0,            # 🔴 KEY FIX (disables pin_memory path)
+    device=0,             
+    workers=0,            
     verbose=True,
     project="runs/detect",
     name="baseline_rdd2020",
@@ -48,5 +40,6 @@ def main():
 
 
 if __name__ == "__main__":
-    freeze_support()   # REQUIRED on Windows
+    freeze_support()   
     main()
+
